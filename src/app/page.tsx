4aea6159516a375affcaeb28f533e30a8d5aa2aa
isai7710 <isai10sanchez@gmail.com>
2024-10-01 16:41:39 -0600

    centered product grid and page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import ProductGrid from "@/components/product-grid";
 export default async function Home() {
   return (
     <div className="flex p-8 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col items-start">
+      <main className="flex flex-col px-2 mx-auto items-start">
         <p>
           Testing WooCommerce API (read-only for now) by displaying products
           below:
