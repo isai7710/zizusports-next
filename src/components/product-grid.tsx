e7d5d9710851064808cb0affd1578d2d5afd2ba8
isai7710 <isai10sanchez@gmail.com>
2024-10-01 16:01:52 -0600

    restructured each card in grid to show image from cloudinary along with price under title

--- src/components/product-grid.tsx
+++ src/components/product-grid.tsx
@@ -1,19 +1,45 @@
+// app/gallery/page.tsx
 import { getWooCommerceProducts } from "@/lib/woocommerce";
+import Image from "next/image";
 import { WooCommerceProduct } from "@/lib/types/woocommerce";
 
 export default async function ProductGrid() {
   const products = await getWooCommerceProducts();
+
   return (
-    <div className="grid grid-cols-2 gap-4 mt-8">
-      {products.length > 0 ? (
-        products.map((product: WooCommerceProduct) => (
-          <div key={product.id} className="border p-4 rounded shadow">
-            <h2 className="text-lg font-bold">{product.name}</h2>
-          </div>
-        ))
-      ) : (
-        <p>No products available</p>
-      )}
-    </div>
+    <>
+      <div className="grid grid-cols-3 gap-4 mt-8">
+        {products.length > 0 ? (
+          products.map((product: WooCommerceProduct) => (
+            <div
+              key={product.id}
+              className="flex justify-between border p-4 rounded shadow"
+            >
+              <div>
+                <h2 className="text-md font-bold max-w-[200px] text-wrap">
+                  {product.name}
+                </h2>
+                <p className="font-light text-sm">Price: ${product.price}</p>
+              </div>
+              <div>
+                {product.images && product.images.length > 0 ? (
+                  <Image
+                    key={product.images[0].id}
+                    src={`https://res.cloudinary.com/de463zyga/image/upload/c_auto/${product.images[0].name}.png`}
+                    alt={product.images[0].alt}
+                    width={100}
+                    height={100}
+                  />
+                ) : (
+                  <p>No images available</p> // Fallback message if no images
+                )}
+              </div>
+            </div>
+          ))
+        ) : (
+          <p>No products available</p>
+        )}
+      </div>
+    </>
   );
 }
