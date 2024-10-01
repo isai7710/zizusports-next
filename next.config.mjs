4ca584dfb0d7659b5a97774b9ddb23a097c2d6c0
isai7710 <isai10sanchez@gmail.com>
2024-10-01 12:08:39 -0600

    configured to safely accept cloudinary URL patterns for fetching and optimizing images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "**", // allows images from all paths in Cloudinary
+      },
+    ],
+  },
+};
 
 export default nextConfig;
