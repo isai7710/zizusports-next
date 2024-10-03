c930aa515e99494488ba14439ed23565c378c89b
isai7710 <isai10sanchez@gmail.com>
2024-10-03 16:08:22 -0600

    moved product grid layout to shop page

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,3 +1,15 @@
+import ProductGrid from "@/components/product-grid";
+
 export default function Shop() {
-  return <div className="min-h-screen">Shop</div>;
+  return (
+    <div className="min-h-screen">
+      <section className="flex flex-col mt-6 px-2 mx-auto items-start">
+        <p>
+          Testing WooCommerce API (read-only for now) by displaying products
+          below:
+        </p>
+        <ProductGrid />
+      </section>
+    </div>
+  );
 }
