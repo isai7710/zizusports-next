a3f0045e489f1919fdd48a4e2f44b64fb274908c
isai7710 <isai10sanchez@gmail.com>
2024-10-03 16:08:07 -0600

    quick first draft of landing page with title for brand

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
-import ProductGrid from "@/components/product-grid";
-
 export default async function Home() {
   return (
-    <main className="flex flex-col p-8 font-[family-name:var(--font-geist-sans)]">
-      <section className="flex flex-col mt-20 px-2 mx-auto items-start">
-        <p>
-          Testing WooCommerce API (read-only for now) by displaying products
-          below:
+    <main className="min-h-screen bg-hero-img bg-center bg-cover bg-no-repeat flex flex-col justify-end">
+      <section className="h-64 w-full max-w-5xl mx-auto flex flex-col justify-center items-start gap-2">
+        <h2 className="mt-10 text-5xl text-bold text-start text-white w-1/3">
+          Powered By Community.
+        </h2>
+        <p className="text-xl text-white">
+          Sportswear with a heartbeat. Your passion, our gear
         </p>
-        <ProductGrid />
+        <button className="text-white font-bold my-1 px-4 rounded-md bg-zinc-100 bg-opacity-25 backdrop-blur-sm transition duration-200 hover:scale-105">
+          Find your team kit here
+        </button>
       </section>
     </main>
   );
