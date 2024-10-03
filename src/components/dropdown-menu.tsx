0ad9bb000c3f2e6944b9bc903880e4e244bb02f2
isai7710 <isai10sanchez@gmail.com>
2024-10-03 16:37:34 -0600

    fixed transparency issue along with text color

--- src/components/dropdown-menu.tsx
+++ src/components/dropdown-menu.tsx
@@ -15,17 +15,17 @@ export default function DropdownMenu({ links, setIsOpen }: DropdownMenuProps) {
   const pathname = usePathname();
 
   return (
-    <div className="absolute right-0 mt-4 w-48 backdrop-blur-sm rounded-md shadow-lg py-1">
+    <div className="absolute right-0 text-white mt-4 w-48 bg-zinc-200 bg-opacity-80 backdrop-blur-sm rounded-md shadow-lg py-1">
       {links.map((link) => (
         <Link
           key={link.name}
           href={link.href}
           onClick={() => setIsOpen(false)}
           className={clsx(
-            "block font-medium px-4 py-2 text-zinc-800",
+            "block font-medium px-4 py-2",
             pathname === link.href
-              ? "text-zinc-800 bg-zinc-200 bg-opacity-10 underline underline-offset-2"
-              : "text-zinc-500 hover:underline hover:bg-zinc-500 hover:bg-opacity-10 hover:underline-offset-2 hover:shadow-inner",
+              ? "text-black bg-zinc-200 bg-opacity-10 underline underline-offset-2"
+              : "text-zinc-500 hover:bg-zinc-500 hover:bg-opacity-10 ",
           )}
         >
           {link.name}
