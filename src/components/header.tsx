c24caa88eab0d0ab27c6a55a05ddcbf480dc6281
isai7710 <isai10sanchez@gmail.com>
2024-10-03 16:08:40 -0600

    reorganized links and added Sizu logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,27 +1,29 @@
 "use client";
 
 import Link from "next/link";
+import Image from "next/image";
 import { usePathname } from "next/navigation";
-import { Menu, X, Search, Home } from "react-feather";
+import { Menu, X, Search, ShoppingCart } from "react-feather";
 import { useState } from "react";
+import logo from "../../public/sizulogo.png";
 import clsx from "clsx";
 import DropdownMenu from "@/components/dropdown-menu";
 
 const links = [
   {
-    name: "Home",
+    name: "HOME",
     href: "/",
   },
   {
-    name: "Shop",
+    name: "SHOP",
     href: "/shop",
   },
   {
-    name: "Contact Us",
+    name: "CONTACT US",
     href: "/contact",
   },
   {
-    name: "About",
+    name: "ABOUT",
     href: "/about",
   },
 ];
@@ -29,48 +31,54 @@ const links = [
 export default function NavBar() {
   const pathname = usePathname();
   const [showDropDown, setShowDropDown] = useState(false);
+  const [searchIsOpen, setSearchIsOpen] = useState(false);
 
   return (
-    <header className="sticky top-0 z-50 w-full bg-zinc-400 bg-opacity-25 border-zinc-100 border shadow-xl backdrop-blur-sm transition-all duration-200">
-      <nav className="flex items-center h-16 px-4 max-w-7xl mx-auto">
-        <div className="flex-1 flex jusify-start items-center">
+    <header className="fixed left-0 top-0 z-50 w-full bg-zinc-100 bg-opacity-80 backdrop-blur-sm">
+      <nav className="flex items-center justify-between h-16 px-4 w-full">
+        <div className="flex jusify-start items-center space-x-10">
           <Link
             href="/"
             className={clsx(
-              "group flex items-center gap-1 text-center font-semibold text-md bg-transparent md:text-xl",
+              "group flex items-center gap-1 text-center font-semibold text-md bg-transparent md:text-4xl",
               pathname === "/" && "text-zinc-800",
             )}
           >
-            <Home className="w-6 h-6 group-hover:rotate-[20deg] transition ease-in-out duration-300" />
-            ZizuSports
+            <Image src={logo} alt="Sizu Logo" className="w-20 bg-transparent" />
           </Link>
+          <ul className="hidden md:flex justify-center space-x-2">
+            {links.map((link) => (
+              <li key={link.name}>
+                <Link
+                  href={link.href}
+                  className={clsx(
+                    "text-sm font-semibold px-2 py-2 rounded-md transition-all duration-200",
+                    pathname === link.href
+                      ? "text-sky-800"
+                      : "text-zinc-500 hover:underline hover:underline-offset-8",
+                  )}
+                >
+                  {link.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
         </div>
 
-        <ul className="hidden md:flex flex-1 items-center justify-center space-x-2">
-          {links.map((link) => (
-            <li key={link.name}>
-              <Link
-                href={link.href}
-                className={clsx(
-                  "text-md font-semibold px-3 py-2 rounded-md transition-all duration-200",
-                  pathname === link.href
-                    ? "text-sky-800 bg-zinc-200 bg-opacity-10 underline underline-offset-4"
-                    : "text-zinc-500 hover:underline hover:bg-zinc-700 hover:bg-opacity-10 hover:underline-offset-2 hover:shadow-xl",
-                )}
-              >
-                {link.name}
-              </Link>
-            </li>
-          ))}
-        </ul>
-
-        <div className="flex-1 flex justify-end items-center gap-1">
-          <Search className="w-6 h-6 text-zinc-800 transition-colors duration-200 hover:text-zinc-400" />
-          <input
-            type="text"
-            className="rounded-lg w-52 placeholder-zinc-300 px-2 py-1 text-sm"
-            placeholder="Search for a product..."
+        <div className="flex items-center gap-1">
+          <Search
+            onClick={() => setSearchIsOpen((prev) => !prev)}
+            className="w-5 h-5 text-zinc-800 transition-colors duration-200 hover:text-zinc-400"
           />
+          {searchIsOpen && (
+            <input
+              type="text"
+              className="rounded-lg w-52 placeholder-zinc-300 px-2 py-1 text-sm"
+              placeholder="Search for a product..."
+              autoFocus={searchIsOpen}
+            />
+          )}
+          <ShoppingCart className="w-5 h-5 text-zinc-800" />
           <div className="relative md:hidden ml-4">
             <button
               onClick={() => setShowDropDown(!showDropDown)}
