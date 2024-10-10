a17d6d096ae020f322c33dcc33e7ff4b05d188b0
isai7710 <isai10sanchez@gmail.com>
2024-10-10 13:55:34 -0600

    fixing header image styling

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -4,14 +4,14 @@ import Image from "next/image";
 export default function Shop() {
   return (
     <>
-      <section className="relative w-full h-48 2xl:h-64 bg-black">
+      <section className="relative w-full h-48 md:h-80 bg-black">
         <Image
           src="https://res.cloudinary.com/de463zyga/image/upload/upslatrslindoor.jpg"
           alt="header image"
           fill
-          className="object-cover object-[0px_51%]"
+          className="object-cover object-[0px_57%]"
         />
-        <h1 className="absolute z-10 text-palette-1 text-4xl font-bold top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 pb-6">
+        <h1 className="absolute text-nowrap z-10 text-palette-1 text-2xl md:text-5xl font-bold top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 pt-32 md:pt-44">
           Gear Up For Your Story.
         </h1>
       </section>
