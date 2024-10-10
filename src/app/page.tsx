670b7558331e1335c78e38876d4eb3d6e3ad0b11
isai7710 <isai10sanchez@gmail.com>
2024-10-10 10:07:47 -0600

    reorganized hero section to include header/description and call to action on one side and carousel on the other

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,32 @@
 import Carousel from "@/components/carousel";
 
-const images = [
-  "uniform22girls.jpg",
-  "uniform22boys1.jpg",
-  "uniform22boys2.jpg",
-  "teamintrainingjerseys.png",
-  "2010inblack.png",
-  "upslatrslindoor.jpg",
-];
-
 export default async function Home() {
+  const images = [
+    "uniform22girls.jpg",
+    "uniform22boys1.jpg",
+    "uniform22boys2.jpg",
+    "teamintrainingjerseys.png",
+    "2010inblack.png",
+    "upslatrslindoor.jpg",
+  ];
+
   return (
-    <main className="bg-palette-1">
+    <main className="bg-palette-1 flex items-center p-2">
+      <div className="w-1/2 pb-4 px-8 h-52 flex flex-col justify-center items-center gap-2 text-primary">
+        <h2 className="text-5xl font-bold text-center  w-full md:w-2/3">
+          Powered By Community.
+        </h2>
+        <p className="w-2/3 md:w-full italic text-center text-xl">
+          Sportswear with a heartbeat. Your passion, our gear.
+        </p>
+        <button className="font-bold my-1 px-4 py-1 rounded-md bg-zinc-400 bg-opacity-25 backdrop-blur-sm transition duration-200 hover:scale-105 hover:-rotate-3">
+          Find your team kit here
+        </button>
+      </div>
       <Carousel
-        className="rounded-xl min-h-[80vh] w-full max-w-5xl mx-auto flex flex-col justify-end"
+        className="rounded-xl min-h-[80vh] w-1/2 mx-auto flex flex-col justify-end"
         imgs={images}
-      >
-        <div className="mb-4 px-8 h-64 flex flex-col justify-end items-center md:items-start gap-2">
-          <h2 className="text-5xl font-bold text-center md:text-start text-white w-full md:w-2/3">
-            Powered By Community.
-          </h2>
-          <p className="w-2/3 md:w-full italic text-center md:text-start text-xl text-white">
-            Sportswear with a heartbeat. Your passion, our gear.
-          </p>
-          <button className="text-white font-bold my-1 px-4 py-1 rounded-md bg-zinc-100 bg-opacity-25 backdrop-blur-sm transition duration-200 hover:scale-105">
-            Find your team kit here
-          </button>
-        </div>
-      </Carousel>
+      />
     </main>
   );
 }
