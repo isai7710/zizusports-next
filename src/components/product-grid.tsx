d1e0b47c8e31c9980465099a81c45d3999859810
isai7710 <isai10sanchez@gmail.com>
2024-10-10 13:42:36 -0600

    added circular color icons to products that have the color attribute and added pointer cursor on hover of products

--- src/components/product-grid.tsx
+++ src/components/product-grid.tsx
@@ -11,30 +11,46 @@ export default async function ProductGrid({
   const products = await getWooCommerceProducts();
 
   return (
-    <section className="w-full max-w-5xl mx-auto my-8 grid grid-cols-2 md:grid-cols-3 gap-x-4 gap-y-8">
+    <section className="w-full max-w-4xl mx-auto my-8 grid grid-cols-2 md:grid-cols-3 gap-x-4 gap-y-8">
       {products.length > 0 ? (
         products.map((product: WooCommerceProduct) => (
           <div
             key={product.id}
-            className={cn(className, "bg-white flex flex-col")}
-          >
-            {product.images && product.images.length > 0 ? (
-              <Image
-                key={product.images[0].id}
-                src={`https://res.cloudinary.com/de463zyga/image/upload/${product.images[0].name}.png`}
-                alt={product.images[0].alt}
-                width={200}
-                height={200}
-                className="min-h-[260px] mx-auto hover:scale-105 transition duration-200 ease-in-out"
-              />
-            ) : (
-              <p>No images available</p>
+            className={cn(
+              className,
+              "group bg-white flex flex-col hover:cursor-pointer",
             )}
-            <div className="flex items-center justify-between px-8">
-              <h2 className="text-lg font-bold max-w-[200px]">
-                {product.name}
-              </h2>
+          >
+            <div className="min-h-[280px] rounded-lg pb-2 flex items-end">
+              {product.images && product.images.length > 0 ? (
+                <Image
+                  key={product.images[0].id}
+                  src={`https://res.cloudinary.com/de463zyga/image/upload/${product.images[0].name}.png`}
+                  alt={product.images[0].alt}
+                  width={200}
+                  height={200}
+                  className="mx-auto group-hover:scale-105 transition duration-200 ease-in-out"
+                />
+              ) : (
+                <p>No images available</p>
+              )}
+            </div>
+            <div className="px-8">
+              <h2 className="text-lg font-bold w-full">{product.name}</h2>
               <p className="font-light text-xs sm:text-sm">${product.price}</p>
+              <div className="mt-2 flex flex-wrap gap-2">
+                {product.attributes &&
+                  product.attributes
+                    .find((attr) => attr.name.toLowerCase() === "color")
+                    ?.options.map((color, index) => (
+                      <span
+                        key={index}
+                        className="w-6 h-6 rounded-full border border-gray-300"
+                        style={{ backgroundColor: color.toLowerCase() }}
+                        title={color}
+                      ></span>
+                    ))}
+              </div>
             </div>
           </div>
         ))
