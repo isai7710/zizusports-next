727fa032d153e4d73383d4304a082a2cac43d16b
isai7710 <isai10sanchez@gmail.com>
2024-10-10 15:13:54 -0600

    removed unecessary relative property to body element

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${saira.className} antialiased relative bg-white`}>
+      <body className={`${saira.className} antialiased bg-white`}>
         <NavBar />
         {children}
         <Footer />
