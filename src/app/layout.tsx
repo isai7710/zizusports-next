6fd5e18a7c71fd61343153c596c45df6bfb265eb
isai7710 <isai10sanchez@gmail.com>
2024-10-10 11:36:40 -0600

    changed background to white

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${saira.className} antialiased relative`}>
+      <body className={`${saira.className} antialiased relative bg-white`}>
         <NavBar />
         {children}
         <Footer />
