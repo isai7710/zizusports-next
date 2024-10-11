88e8bde1254afd6f56990372cadbd614bb41366e
isai7710 <isai10sanchez@gmail.com>
2024-10-11 15:30:00 -0600

    new getWooProductById function that accepts product id as function parameter and returns that product's data if it exists in the backend Woo api

--- src/lib/woocommerce.ts
+++ src/lib/woocommerce.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError } from "axios";
 
-export async function getWooCommerceProducts() {
+export async function getWooProducts() {
   const consumerKey = process.env.WC_CONSUMER_KEY;
   const consumerSecret = process.env.WC_CONSUMER_SECRET;
   const apiUrl = process.env.WC_API_URL;
@@ -20,7 +20,7 @@ export async function getWooCommerceProducts() {
       },
     );
 
-    return response.data; // Returns the products
+    return response.data;
   } catch (error) {
     if (axios.isAxiosError(error)) {
       const axiosError = error as AxiosError;
@@ -37,6 +37,46 @@ export async function getWooCommerceProducts() {
       }
     } else {
       console.error("Error fetching WooCommerce products:", error);
+    }
+
+    throw error;
+  }
+}
+
+export async function getWooProductById(id: string) {
+  const consumerKey = process.env.WC_CONSUMER_KEY;
+  const consumerSecret = process.env.WC_CONSUMER_SECRET;
+  const apiUrl = process.env.WC_API_URL;
+
+  if (!consumerKey || !consumerSecret || !apiUrl) {
+    throw new Error("WooCommerce API credentials are missing");
+  }
+
+  try {
+    const response = await axios.get(`${apiUrl}/products/${id}`, {
+      auth: {
+        username: consumerKey,
+        password: consumerSecret,
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      const axiosError = error as AxiosError;
+      if (axiosError.response) {
+        console.error(
+          "API responded with error:",
+          axiosError.response.status,
+          axiosError.response.data,
+        );
+      } else if (axiosError.request) {
+        console.error("No response received:", axiosError.request);
+      } else {
+        console.error("Error setting up request:", axiosError.message);
+      }
+    } else {
+      console.error("Error fetching WooCommerce product:", error);
     }
 
     throw error;
