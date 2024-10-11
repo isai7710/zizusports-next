e5443254628841cd5e28d7295b235b0b28a9d758
isai7710 <isai10sanchez@gmail.com>
2024-10-11 15:30:58 -0600

    new components to display picker options for basic product attributes like size and color, in the future these will be part of a form that performs shopping cart functionality as an action when user clicks add to cart

---
+++ src/components/shop/color-picker.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { useState } from "react";
+
+export function ColorPicker({ colors }: { colors: string[] }) {
+  const [selectedColor, setSelectedColor] = useState(colors[0]);
+
+  return (
+    <div>
+      <h2 className="text-sm font-medium text-gray-900 mt-8 mb-1">Colors</h2>
+      <div className="flex items-center space-x-3">
+        {colors.map((color, index) => (
+          <span
+            key={index}
+            onClick={() => setSelectedColor(colors[index])}
+            className={cn(
+              color == selectedColor ? "ring ring-offset-1 ring-primary" : "",
+              "w-6 h-6 relative m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 focus:outline-none hover:cursor-pointer",
+            )}
+            style={{ backgroundColor: color.toLowerCase() }}
+            title={color}
+          ></span>
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ src/components/shop/size-picker.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { useState } from "react";
+
+export function SizePicker({ sizes }: { sizes: string[] }) {
+  const [selectedSize, setSelectedSize] = useState(sizes[2]);
+
+  return (
+    <div className="mt-8">
+      <div className="flex items-center justify-between mb-1">
+        <h2 className="text-sm font-medium text-gray-900">Select a size</h2>
+        <a
+          href="#"
+          className="text-sm font-medium text-gray-400 hover:text-slate-500"
+        >
+          See sizing chart
+        </a>
+      </div>
+
+      <div className="grid grid-cols-3 gap-3 sm:grid-cols-6">
+        {sizes.map((size, index) => (
+          <span
+            key={index}
+            onClick={() => setSelectedSize(sizes[index])}
+            className={cn(
+              size === selectedSize
+                ? "border-transparent bg-primary text-white hover:bg-opacity-80 transition duration-100 ease-in-out"
+                : "border-gray-200 bg-white text-gray-900 hover:bg-gray-100 transition duration-100 ease-in-out",
+              "flex items-center justify-center rounded-md border py-1 px-2 text-sm font-medium uppercase sm:flex-1 hover:cursor-pointer",
+            )}
+          >
+            {size}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+}
