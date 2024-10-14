d4f6879d22ab5b4fe1b91713177846d3e75b4fad
isai7710 <isai10sanchez@gmail.com>
2024-10-14 14:28:56 -0600

    sizes utility file to store sizes of different products for sizing chart

---
+++ src/lib/sizes.ts
@@ -0,0 +1,47 @@
+export const jerseySizes = [
+  {
+    size: "YS",
+    chest: 42.5,
+    torso: 59.5,
+  },
+  {
+    size: "YM",
+    chest: 76,
+    torso: 62,
+  },
+  {
+    size: "YL",
+    chest: 81,
+    torso: 67,
+  },
+  {
+    size: "YXL",
+    chest: 86,
+    torso: 71,
+  },
+  {
+    size: "S",
+    chest: 94,
+    torso: 76,
+  },
+  {
+    size: "M",
+    chest: 102,
+    torso: 81,
+  },
+  {
+    size: "L",
+    chest: 102,
+    torso: 81,
+  },
+  {
+    size: "XL",
+    chest: 102,
+    torso: 81,
+  },
+  {
+    size: "2XL",
+    chest: 102,
+    torso: 81,
+  },
+];
