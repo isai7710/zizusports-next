f47a0a3036ff15800e39df757f0b1e17dbad1716
Isai Sanzy <isai10sanchez@gmail.com>
2024-10-24 18:13:13 -0600

    add to cart button component which handles adding items to cart with the addItem cart context function

---
+++ src/components/cart/add-to-cart-button.tsx
@@ -0,0 +1,35 @@
+// app/shop/product/[id]/AddToCartButton.tsx
+"use client";
+
+import { useCart } from "@/components/contexts/cart-context";
+import { WooCommerceProduct } from "@/lib/types/woocommerce";
+
+interface AddToCartButtonProps {
+  product: WooCommerceProduct;
+  selectedColor?: string;
+  selectedSize?: string;
+}
+
+export function AddToCartButton({
+  product,
+  selectedColor,
+  selectedSize,
+}: AddToCartButtonProps) {
+  const { addItem } = useCart();
+
+  const handleAddToCart = () => {
+    addItem(product, 1, {
+      color: selectedColor,
+      size: selectedSize,
+    });
+  };
+
+  return (
+    <button
+      onClick={handleAddToCart}
+      className="mt-8 flex w-full items-center justify-center rounded-md border border-transparent bg-primary px-8 py-3 text-base font-medium text-white hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-offset-2"
+    >
+      Add to cart
+    </button>
+  );
+}
