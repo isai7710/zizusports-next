e1bcc84b927ef5be55ce567df55fd0f6bca16b92
Isai Sanzy <isai10sanchez@gmail.com>
2024-11-01 16:13:46 -0600

    refactored to include all product info and data, additionally this component now shows main product image based on color chosen by user, but I might remove this behavior to instead change main product image to whatever image user selects out of the thumbnail images showing below the main one

--- src/components/shop/product-interaction.tsx
+++ src/components/shop/product-interaction.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import { WooCommerceProduct, ProductImage } from "@/lib/types/woocommerce";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ColorPicker } from "@/components/shop/color-picker";
 import { SizePicker } from "@/components/shop/size-picker";
 import { AddToCartButton } from "@/components/cart/add-to-cart-button";
 import Image from "next/image";
+import { Star } from "react-feather";
+import { cn } from "@/lib/utils";
 
 interface ProductInteractiveSectionProps {
   product: WooCommerceProduct;
@@ -21,49 +23,110 @@ export function ProductInteractiveSection({
   images,
 }: ProductInteractiveSectionProps) {
   const [selectedColor, setSelectedColor] = useState<string | undefined>(
-    colors.length > 0 ? colors[0] : undefined,
+    undefined,
   );
   const [selectedSize, setSelectedSize] = useState<string | undefined>(
     sizes.length > 0 ? sizes[0] : undefined,
   );
+  const [currentImage, setCurrentImage] = useState<ProductImage | undefined>(
+    undefined,
+  );
+
+  useEffect(() => {
+    if (!currentImage && images.length > 0) {
+      setCurrentImage(images[0]);
+      return;
+    }
+
+    const findImageByColor = (color: string | undefined) => {
+      if (!color || !images.length) return undefined;
+
+      // Assuming alt text format like "color:black | Description"
+      return images.find((img) =>
+        img.alt.toLowerCase().includes(color.toLowerCase()),
+      );
+    };
+    // Find and set image matching selected color
+    const matchingImage = findImageByColor(selectedColor);
+    if (matchingImage) {
+      setCurrentImage(matchingImage);
+    }
+  }, [selectedColor, images, currentImage]);
 
   return (
-    <>
-      <div className="md:col-span-5 md:col-start-1 md:row-span-3 md:row-start-1">
+    <section className="md:grid md:grid-cols-8 md:auto-rows-min md:gap-x-6">
+      <div className="md:col-span-4 md:col-start-1 md:row-start-1">
         <div className="relative w-full aspect-[3/4] md:aspect-[4/5] md:overflow-hidden">
-          {images && images.length > 0 ? (
+          {currentImage && (
             <Image
-              key={images[0].id}
-              src={`https://res.cloudinary.com/de463zyga/image/upload/${product.images[0].name}.png`}
-              alt={images[0].alt}
+              key={currentImage.id}
+              src={`https://res.cloudinary.com/de463zyga/image/upload/${currentImage.name}.png`}
+              alt={currentImage.alt}
               fill
               sizes="(max-width: 768px) 50vw, (max-width: 1200px) 33vw, 25vw"
               className="object-cover rounded-xl"
             />
-          ) : (
-            <div className="w-full h-full flex items-center justify-center bg-gray-200">
-              <p className="text-gray-500">No image</p>
-            </div>
           )}
         </div>
-        <div className="w-full mt-4 grid grid-cols-4 gap-x-2">
+        <div className="mt-2 grid grid-cols-4 gap-2">
           {images.map((img, index) => (
             <div
               key={index}
-              className="relative w-24 aspect-[3/4] overflow-hidden"
+              className="relative w-full aspect-square overflow-hidden"
             >
               <Image
                 src={`https://res.cloudinary.com/de463zyga/image/upload/${img.name}.png`}
                 alt={img.alt}
                 fill
-                className="object-fit rounded-md"
+                className="object-cover rounded-md"
               />
             </div>
           ))}
         </div>
       </div>
 
-      <div className="md:col-span-5">
+      <div className="md:col-span-4 md:col-start-5">
+        <div className="mt-2 md:mt-0 flex md:flex-col justify-between">
+          <h1 className="text-2xl md:text-4xl font-medium text-gray-900">
+            {product.name}
+          </h1>
+          <p className="text-xl font-medium text-gray-900">${product.price}</p>
+        </div>
+        <div className="mt-2 md:mt-4 mb-4 md:mb-0">
+          <h2 className="sr-only">Reviews</h2>
+          <div className="flex items-center">
+            {/* TODO: add reviews attribute when fetching product data*/}
+            <div className="mr-2 flex items-center">
+              {[0, 1, 2, 3, 4].map((rating) => (
+                <Star
+                  key={rating}
+                  className={cn(
+                    4 > rating ? "text-yellow-400" : "text-gray-200",
+                    "h-5 w-5 flex-shrink-0",
+                  )}
+                  aria-hidden="true"
+                />
+              ))}
+            </div>
+            <p className="text-sm text-gray-700">
+              {/* TODO: add reviews attribute when fetching product data*/}
+              {4}
+              <span className="sr-only"> out of 5 stars</span>
+            </p>
+            <div aria-hidden="true" className="ml-4 text-sm text-gray-300">
+              ·
+            </div>
+            <div className="ml-4 flex">
+              <a
+                href="#"
+                className="text-sm font-medium text-slate-600 hover:text-slate-500"
+              >
+                See all {42} reviews
+              </a>
+            </div>
+          </div>
+        </div>
+
         <form onSubmit={(e) => e.preventDefault()}>
           <div className="md:mt-8 md:mb-1 flex flex-col gap-3">
             {colors.length > 0 && (
@@ -91,6 +154,6 @@ export function ProductInteractiveSection({
           />
         </form>
       </div>
-    </>
+    </section>
   );
 }
