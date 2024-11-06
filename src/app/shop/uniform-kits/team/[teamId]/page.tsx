b826ea1b7dcf2bbf4ad38959cfb8c3899d744ea1
Isai Sanzy <isai10sanchez@gmail.com>
2024-11-06 16:42:04 -0700

    added params prop to access teamId from url and pass it into a server action that returns team information to be displayed on the page through the interactive component

--- src/app/shop/uniform-kits/team/[teamId]/page.tsx
+++ src/app/shop/uniform-kits/team/[teamId]/page.tsx
@@ -1,16 +1,37 @@
 import { getWooProductById, getWooProductsByTagId } from "@/lib/woocommerce";
-import { WooCommerceProduct } from "@/lib/types/woocommerce";
+import { getTeamInfoById } from "@/lib/supabase";
 import { KitInteractiveSection } from "@/components/shop/uniform-kits/kit-interaction";
 
-export default async function KitPage() {
-  const kit: WooCommerceProduct = await getWooProductById("1276");
-  const products: WooCommerceProduct[] = await getWooProductsByTagId("34");
-
+export default async function KitPage({
+  params,
+}: {
+  params: { teamId: string };
+}) {
+  const kit = await getWooProductById("1276");
   const kitImages = kit.images?.length ? kit.images : [];
+  const products = await getWooProductsByTagId("34");
+
+  const { success, team, players } = await getTeamInfoById(params.teamId);
+
+  if (!success || !team) {
+    console.log(success, team);
+    return (
+      <p>
+        Error loading team information. Please check the team code or try again.
+      </p>
+    );
+  }
 
   return (
     <main className="min-h-screen bg-gradient-to-br from-white-50 to-gray-300 p-8">
-      <KitInteractiveSection kit={kit} images={kitImages} products={products} />
+      <KitInteractiveSection
+        kit={kit}
+        images={kitImages}
+        products={products}
+        teamName={team.name}
+        players={players}
+        clubName={"Real Madrid"}
+      />
 
       {kit.short_description && (
         <div className="mt-4 md:mt-8 flex flex-col gap-2">
