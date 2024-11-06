f58cbe7db8b515f567ddb63d59b60ab31a7f1c53
Isai Sanzy <isai10sanchez@gmail.com>
2024-11-05 17:40:17 -0700

    page now displays main kit images and title in a format similar to individual product pages in the shop/gear/product/[id] route but it also fetches products with the tagid corresponding to uniform_kits and renders cards for each product with this tag (jersey, shorts, socks) that displays the main image, title, price, and selection element that lets user select size (size options correspond to size attribute for each product)

--- src/app/shop/uniform-kits/team/[teamId]/page.tsx
+++ src/app/shop/uniform-kits/team/[teamId]/page.tsx
@@ -1,37 +1,21 @@
-import { getWooProductById } from "@/lib/woocommerce";
+import { getWooProductById, getWooProductsByTagId } from "@/lib/woocommerce";
 import { WooCommerceProduct } from "@/lib/types/woocommerce";
-import { ProductInteractiveSection } from "@/components/shop/product-interaction";
+import { KitInteractiveSection } from "@/components/shop/uniform-kits/kit-interaction";
 
-export default async function ProductPage() {
-  const product: WooCommerceProduct = await getWooProductById("1276");
+export default async function KitPage() {
+  const kit: WooCommerceProduct = await getWooProductById("1276");
+  const products: WooCommerceProduct[] = await getWooProductsByTagId("34");
 
-  const images = product.images?.length ? product.images : [];
-
-  // Find the color and sizes attribute in the product attributes if it exists
-  const colorAttribute = product.attributes.find(
-    (attr) => attr.name.toLowerCase() === "color",
-  );
-  const sizeAttribute = product.attributes.find(
-    (attr) => attr.name.toLowerCase() === "size",
-  );
-
-  // If the color and size attributes exist, extract the options of that attribute
-  const colors = colorAttribute ? colorAttribute.options : [];
-  const sizes = sizeAttribute ? sizeAttribute.options : [];
+  const kitImages = kit.images?.length ? kit.images : [];
 
   return (
-    <main className="mx-auto mt-6 md:mt-8 max-w-xl px-4 pb-16 sm:px-6 sm:pb-24 md:max-w-4xl md:px-8 2xl:max-w-5xl">
-      <ProductInteractiveSection
-        product={product}
-        colors={colors}
-        sizes={sizes}
-        images={images}
-      />
+    <main className="mx-auto mt-6 md:mt-8 max-w-xl px-4 pb-16 sm:px-6 sm:pb-24 md:max-w-5xl md:px-8">
+      <KitInteractiveSection kit={kit} images={kitImages} products={products} />
 
-      {product.short_description && (
+      {kit.short_description && (
         <div className="mt-4 md:mt-8 flex flex-col gap-2">
           <h2 className="text-xl font-semibold">Description</h2>
-          <p>{product.short_description.replace(/<\/?p>/g, "")}</p>
+          <p>{kit.short_description.replace(/<\/?p>/g, "")}</p>
         </div>
       )}
     </main>
