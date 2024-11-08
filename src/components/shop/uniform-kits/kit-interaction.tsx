a12c4a0823dc02066dc526f56bd453bb0e594bb4
Isai Sanzy <isai10sanchez@gmail.com>
2024-11-08 08:46:56 -0700

    accepts new props and also has updated add to cart button

--- src/components/shop/uniform-kits/kit-interaction.tsx
+++ src/components/shop/uniform-kits/kit-interaction.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { WooCommerceProduct, ProductImage } from "@/lib/types/woocommerce";
+import { WooCommerceProduct } from "@/lib/types/woocommerce";
 import { Player } from "@/lib/types/supabase";
 import { useState } from "react";
-import { AddToCartButton } from "@/components/cart/add-to-cart-button";
+import { AddKitToCartButton } from "@/components/cart/add-to-cart-button";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
 import {
@@ -15,8 +15,9 @@ import {
 } from "@/components/ui/select";
 
 interface KitInteractiveSectionProps {
-  kit: WooCommerceProduct;
-  images: ProductImage[];
+  kitName: string;
+  kitPrice: string;
+  images: string[];
   products: WooCommerceProduct[];
   teamName: string;
   players: Player[];
@@ -24,22 +25,21 @@ interface KitInteractiveSectionProps {
 }
 
 export function KitInteractiveSection({
-  kit,
+  kitName,
+  kitPrice,
   images,
   products,
   teamName,
   players,
   clubName,
 }: KitInteractiveSectionProps) {
-  const [currentImage, setCurrentImage] = useState<ProductImage | undefined>(
-    images.length > 0 ? images[0] : undefined,
-  );
-  const [selectedProducts, setSelectedProducts] = useState<{
-    [key: number]: string;
-  }>({});
+  const [currentImage, setCurrentImage] = useState<string>(images[0] || "");
+  const [selectedProducts, setSelectedProducts] = useState<
+    Record<number, string>
+  >({});
   const [selectedPlayer, setSelectedPlayer] = useState<string>("");
-  const [selectedColor, setSelectedColor] = useState<"black" | "white">(
-    "black",
+  const [selectedColor, setSelectedColor] = useState<"Black" | "White">(
+    "Black",
   );
 
   const handleSizeChange = (productId: number, size: string) => {
@@ -51,8 +51,8 @@ export function KitInteractiveSection({
       <div className="flex flex-col md:grid md:grid-cols-11 md:gap-x-6">
         <div className="block md:hidden">
           <div className="flex justify-between items-end mb-1">
-            <h1 className="text-2xl font-semibold text-gray-900">{kit.name}</h1>
-            <h2 className="text-xl font-medium text-primary">${kit.price}</h2>
+            <h1 className="text-2xl font-semibold text-gray-900">{kitName}</h1>
+            <h2 className="text-xl font-medium text-primary">${kitPrice}</h2>
           </div>
           <p className="text-lg mb-2">
             {clubName} - {teamName}
@@ -72,7 +72,7 @@ export function KitInteractiveSection({
                 key={index}
                 onClick={() => setCurrentImage(img)}
                 className={cn(
-                  currentImage?.id === img.id
+                  currentImage === img
                     ? "border-2 border-primary/20"
                     : "border-none",
                   "relative w-16 md:w-full aspect-[2/3] flex-shrink-0 md:flex-shrink",
@@ -80,8 +80,8 @@ export function KitInteractiveSection({
                 )}
               >
                 <Image
-                  src={`https://res.cloudinary.com/de463zyga/image/upload/${img.name}.png`}
-                  alt={img.alt}
+                  src={`https://res.cloudinary.com/de463zyga/image/upload/${img}.png`}
+                  alt={img}
                   fill
                   className="object-cover"
                 />
@@ -94,9 +94,9 @@ export function KitInteractiveSection({
           <div className="relative w-full aspect-square md:aspect-[2/3] overflow-hidden rounded-xl">
             {currentImage && (
               <Image
-                key={currentImage.id}
-                src={`https://res.cloudinary.com/de463zyga/image/upload/${currentImage.name}.png`}
-                alt={currentImage.alt}
+                key={currentImage}
+                src={`https://res.cloudinary.com/de463zyga/image/upload/${currentImage}.png`}
+                alt={currentImage}
                 fill
                 sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                 className="object-cover"
@@ -109,10 +109,10 @@ export function KitInteractiveSection({
           <div className="hidden md:block">
             <div className="flex md:justify-between items-end mb-1">
               <h1 className="text-2xl md:text-3xl font-semibold text-gray-900">
-                {kit.name}
+                {kitName}
               </h1>
               <h2 className="text-xl md:text-2xl font-medium text-primary">
-                ${kit.price}
+                ${kitPrice}
               </h2>
             </div>
             <p className="text-lg">
@@ -120,90 +120,104 @@ export function KitInteractiveSection({
             </p>
           </div>
 
-          <div className="flex items-center justify-between my-6">
-            <div>
-              <label className="block text-sm font-medium text-gray-700 mb-1">
-                Kit Color
-              </label>
-              <div className="flex space-x-4">
-                <button
-                  onClick={() => setSelectedColor("black")}
-                  className={`w-8 h-8 rounded-full bg-black ${selectedColor === "black" ? "ring-2 ring-blue-500 ring-offset-2" : ""}`}
-                  aria-label="Black"
-                />
-                <button
-                  onClick={() => setSelectedColor("white")}
-                  className={`w-8 h-8 rounded-full bg-white border border-gray-300 ${selectedColor === "white" ? "ring-2 ring-blue-500 ring-offset-2" : ""}`}
-                  aria-label="White"
-                />
+          <form onSubmit={(e) => e.preventDefault()} className="space-y-6">
+            <div className="flex items-center justify-between my-6">
+              <div>
+                <label className="block text-sm font-medium text-gray-700 mb-1">
+                  Kit Color
+                </label>
+                <div className="flex space-x-4">
+                  <button
+                    onClick={() => setSelectedColor("Black")}
+                    className={`w-8 h-8 rounded-full bg-black ${selectedColor === "Black" ? "ring-2 ring-blue-500 ring-offset-2" : ""}`}
+                    aria-label="Black"
+                  />
+                  <button
+                    onClick={() => setSelectedColor("White")}
+                    className={`w-8 h-8 rounded-full bg-white border border-gray-300 ${selectedColor === "White" ? "ring-2 ring-blue-500 ring-offset-2" : ""}`}
+                    aria-label="White"
+                  />
+                </div>
               </div>
-            </div>
-            <div>
-              <label
-                htmlFor="player-select"
-                className="block text-sm font-medium text-gray-700 mb-1"
-              >
-                Select Player
-              </label>
-              <Select onValueChange={setSelectedPlayer} value={selectedPlayer}>
-                <SelectTrigger id="player-select" className="w-full">
-                  <SelectValue placeholder="Choose a player" />
-                </SelectTrigger>
-                <SelectContent>
-                  {players.map((player) => (
-                    <SelectItem key={player.id} value={player.id}>
-                      {player.name}
-                    </SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
-            </div>
-          </div>
-
-          <div className="my-6 space-y-4">
-            {products.map((product) => (
-              <div
-                key={product.id}
-                className="flex items-center space-x-4 bg-white p-4 rounded-lg shadow-sm border border-gray-200"
-              >
-                <div className="relative w-16 h-16 flex-shrink-0">
-                  {product.images.length > 0 && (
-                    <Image
-                      src={`https://res.cloudinary.com/de463zyga/image/upload/${product.images[0].name}`}
-                      alt={product.images[0].alt}
-                      fill
-                      className="object-cover rounded-md"
-                    />
-                  )}
-                </div>
-                <div className="flex-grow">
-                  <h3 className="text-sm font-medium text-gray-900">
-                    {product.name}
-                  </h3>
-                  <p className="text-sm text-gray-500">${product.price}</p>
-                </div>
-                <Select
-                  onValueChange={(value) => handleSizeChange(product.id, value)}
-                  value={selectedProducts[product.id] || ""}
+              <div>
+                <label
+                  htmlFor="player-select"
+                  className="block text-sm font-medium text-gray-700 mb-1"
                 >
-                  <SelectTrigger className="w-[100px]">
-                    <SelectValue placeholder="Size" />
+                  Select Player
+                </label>
+                <Select
+                  onValueChange={setSelectedPlayer}
+                  value={selectedPlayer}
+                >
+                  <SelectTrigger id="player-select" className="w-full">
+                    <SelectValue placeholder="Choose a player" />
                   </SelectTrigger>
                   <SelectContent>
-                    {product.attributes
-                      .find((attr) => attr.name.toLowerCase() === "size")
-                      ?.options.map((size) => (
-                        <SelectItem key={size} value={size}>
-                          {size}
-                        </SelectItem>
-                      ))}
+                    {players.map((player) => (
+                      <SelectItem key={player.id} value={player.id}>
+                        {player.name}
+                      </SelectItem>
+                    ))}
                   </SelectContent>
                 </Select>
               </div>
-            ))}
-          </div>
-          <form onSubmit={(e) => e.preventDefault()} className="space-y-6">
-            <AddToCartButton product={kit} />
+            </div>
+
+            <div className="my-6 space-y-4">
+              {products.map((product) => (
+                <div
+                  key={product.id}
+                  className="flex items-center space-x-4 bg-white p-4 rounded-lg shadow-sm border border-gray-200"
+                >
+                  <div className="relative w-16 h-16 flex-shrink-0">
+                    {product.images.length > 0 && (
+                      <Image
+                        src={`https://res.cloudinary.com/de463zyga/image/upload/${product.images[0].name}`}
+                        alt={product.images[0].alt}
+                        fill
+                        className="object-cover rounded-md"
+                      />
+                    )}
+                  </div>
+                  <div className="flex-grow">
+                    <h3 className="text-sm font-medium text-gray-900">
+                      {product.name}
+                    </h3>
+                    <p className="text-sm text-gray-500">${product.price}</p>
+                  </div>
+                  <Select
+                    onValueChange={(value) =>
+                      handleSizeChange(product.id, value)
+                    }
+                    value={selectedProducts[product.id] || ""}
+                  >
+                    <SelectTrigger className="w-[100px]">
+                      <SelectValue placeholder="Size" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {product.attributes
+                        .find((attr) => attr.name.toLowerCase() === "size")
+                        ?.options.map((size) => (
+                          <SelectItem key={size} value={size}>
+                            {size}
+                          </SelectItem>
+                        ))}
+                    </SelectContent>
+                  </Select>
+                </div>
+              ))}
+            </div>
+            <AddKitToCartButton
+              kitName={kitName}
+              kitPrice={kitPrice}
+              kitImage={currentImage}
+              selectedPlayer={selectedPlayer}
+              selectedColor={selectedColor}
+              selectedProducts={selectedProducts}
+              teamName={teamName}
+              products={products}
+            />
           </form>
         </div>
       </div>
