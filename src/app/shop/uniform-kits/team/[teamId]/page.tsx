5c86743f7da48f9268a84d6dfd30f57cb52f837b
Isai Sanzy <isai10sanchez@gmail.com>
2024-11-08 01:12:45 -0700

    updated props that will be drilled down into KitInteractiveSection component ,maybe there's a more efficient way to do this

--- src/app/shop/uniform-kits/team/[teamId]/page.tsx
+++ src/app/shop/uniform-kits/team/[teamId]/page.tsx
@@ -1,16 +1,18 @@
 import { getWooProductById, getWooProductsByTagId } from "@/lib/woocommerce";
 import { getTeamInfoById } from "@/lib/supabase";
 import { KitInteractiveSection } from "@/components/shop/uniform-kits/kit-interaction";
+import { WooCommerceProduct } from "@/lib/types/woocommerce";
 
 export default async function KitPage({
   params,
 }: {
   params: { teamId: string };
 }) {
-  const kit = await getWooProductById("1276");
-  const kitImages = kit.images?.length ? kit.images : [];
-  const products = await getWooProductsByTagId("34");
-
+  const kitInfo: WooCommerceProduct = await getWooProductById("1276");
+  const kitName = kitInfo.name;
+  const kitPrice = kitInfo.price;
+  const kitImages = kitInfo.images?.map((img) => img.name);
+  const kitProducts = await getWooProductsByTagId("34");
   const { success, team, players } = await getTeamInfoById(params.teamId);
 
   if (!success || !team) {
@@ -25,18 +27,19 @@ export default async function KitPage({
   return (
     <main className="min-h-screen bg-gradient-to-br from-white-50 to-gray-300 p-8">
       <KitInteractiveSection
-        kit={kit}
+        kitName={kitName}
+        kitPrice={kitPrice}
         images={kitImages}
-        products={products}
+        products={kitProducts}
         teamName={team.name}
         players={players}
         clubName={"Real Madrid"}
       />
 
-      {kit.short_description && (
+      {kitInfo.short_description && (
         <div className="mt-4 md:mt-8 flex flex-col gap-2">
           <h2 className="text-xl font-semibold">Description</h2>
-          <p>{kit.short_description.replace(/<\/?p>/g, "")}</p>
+          <p>{kitInfo.short_description.replace(/<\/?p>/g, "")}</p>
         </div>
       )}
     </main>
