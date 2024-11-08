11efdc10450f70902de1033f2498323ba7b13545
Isai Sanzy <isai10sanchez@gmail.com>
2024-11-08 01:14:25 -0700

    there are now two different buttons that handle adding individual products (gear) to cart and kit products to cart, this will be further optimized soon but its functional now

--- src/components/cart/add-to-cart-button.tsx
+++ src/components/cart/add-to-cart-button.tsx
@@ -1,26 +1,106 @@
 "use client";
 
-import { useCart } from "@/components/cart/cart-context";
+import { KitCartItem, useCart } from "@/components/cart/cart-context";
 import { WooCommerceProduct } from "@/lib/types/woocommerce";
 
-interface AddToCartButtonProps {
+interface AddProductToCartButtonProps {
   product: WooCommerceProduct;
   selectedColor?: string;
   selectedSize?: string;
 }
 
-export function AddToCartButton({
+export function AddProductToCartButton({
   product,
   selectedColor,
   selectedSize,
-}: AddToCartButtonProps) {
-  const { addItem, toggleModal } = useCart();
+}: AddProductToCartButtonProps) {
+  const { addProductItem, toggleModal } = useCart();
 
   const handleAddToCart = () => {
-    addItem(product, 1, {
+    addProductItem(product, 1, {
       color: selectedColor,
       size: selectedSize,
     });
+    toggleModal();
+  };
+
+  return (
+    <button
+      onClick={handleAddToCart}
+      className="mt-8 flex w-full items-center justify-center rounded-md border border-transparent bg-primary px-8 py-3 text-base font-medium text-white hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-offset-2"
+    >
+      Add to cart
+    </button>
+  );
+}
+
+interface AddKitToCartButtonProps {
+  kitName: string;
+  kitPrice: string;
+  kitImage: string;
+  selectedPlayer: string;
+  selectedColor: "Black" | "White";
+  selectedProducts: Record<number, string>;
+  products: WooCommerceProduct[];
+  teamName: string;
+}
+
+export function AddKitToCartButton({
+  kitName,
+  kitPrice,
+  selectedPlayer,
+  selectedColor,
+  selectedProducts,
+  teamName,
+  products,
+}: AddKitToCartButtonProps) {
+  const { addKitItem, toggleModal } = useCart();
+
+  const handleAddToCart = () => {
+    console.log("in handle add to cart func");
+    const jerseyProduct = products.find((product) =>
+      product.name.toLowerCase().includes("jersey"),
+    );
+    const shortsProduct = products.find((product) =>
+      product.name.toLowerCase().includes("shorts"),
+    );
+    const socksProduct = products.find((product) =>
+      product.name.toLowerCase().includes("socks"),
+    );
+
+    console.log(selectedProducts[1]);
+
+    if (!jerseyProduct || !shortsProduct || !socksProduct) {
+      console.error("Missing product details for kit components!");
+      return;
+    }
+    // Create a KitCartItem
+    const kitCartItem: KitCartItem = {
+      id: parseInt(
+        `${selectedProducts[1]}${selectedProducts[2]}${selectedProducts[3]}${selectedColor}`,
+      ), // Generate a unique ID for the kit (based on product IDs and color)
+      name: kitName,
+      img: jerseyProduct.images[0]?.name || "", // Assuming the jersey image is used for kit image
+      jersey: {
+        product: jerseyProduct,
+        size: selectedProducts[jerseyProduct.id], // selected size for jersey
+      },
+      shorts: {
+        product: shortsProduct,
+        size: selectedProducts[shortsProduct.id], // selected size for shorts
+      },
+      socks: {
+        product: socksProduct,
+        size: selectedProducts[socksProduct.id], // selected size for socks
+      },
+      globalColor: selectedColor,
+      price: parseFloat(kitPrice),
+      player: selectedPlayer,
+      team: teamName,
+      quantity: 1,
+    };
+
+    addKitItem(kitCartItem, 1);
     toggleModal();
   };
 
