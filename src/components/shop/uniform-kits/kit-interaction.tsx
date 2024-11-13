77136508899332a608015b5c1ed89ae052a33142
Isai Sanzy <isai10sanchez@gmail.com>
2024-11-13 13:38:55 -0700

    moved section element to parent component

--- src/components/shop/uniform-kits/kit-interaction.tsx
+++ src/components/shop/uniform-kits/kit-interaction.tsx
@@ -83,210 +83,208 @@ export function KitInteractiveSection(props: KitInfo) {
   };
 
   return (
-    <section className="max-w-5xl mx-auto bg-white rounded-2xl shadow-2xl overflow-hidden p-4">
-      <div className="flex flex-col md:grid md:grid-cols-11 md:gap-x-6">
-        <div className="block md:hidden">
-          <div className="flex justify-between items-end mb-1">
-            <h1 className="text-2xl font-semibold text-gray-900">{kitName}</h1>
-            <h2 className="text-xl font-medium text-primary">${kitPrice}</h2>
+    <div className="flex flex-col md:grid md:grid-cols-11 md:gap-x-6">
+      <div className="block md:hidden">
+        <div className="flex justify-between items-end mb-1">
+          <h1 className="text-2xl font-semibold text-gray-900">{kitName}</h1>
+          <h2 className="text-xl font-medium text-primary">${kitPrice}</h2>
+        </div>
+        <p className="text-lg mb-2">
+          {clubName} - {teamName}
+        </p>
+      </div>
+
+      <div className="order-2 md:order-1 md:col-span-1 md:col-start-1 md:row-start-1">
+        <div
+          className={cn(
+            "flex md:grid md:grid-rows-4 gap-2",
+            "overflow-x-auto md:overflow-x-visible",
+            "my-4 md:my-0 space-x-2 md:space-x-0",
+          )}
+        >
+          {images.map((img, index) => (
+            <div
+              key={index}
+              onClick={() => setCurrentImage(img)}
+              className={cn(
+                currentImage === img
+                  ? "border border-primary/30 shadow-md"
+                  : "border border-gray-200",
+                "relative w-16 md:w-full aspect-square flex-shrink-0 md:flex-shrink",
+                "overflow-hidden rounded-md cursor-pointer",
+              )}
+            >
+              <Image
+                src={`https://res.cloudinary.com/de463zyga/image/upload/${img}.png`}
+                alt={img}
+                fill
+                className="object-contain"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="order-1 md:order-2 md:col-span-5 md:col-start-2 md:row-start-1">
+        <div className="relative w-full aspect-square md:aspect-[2/3] overflow-hidden rounded-xl">
+          {currentImage && (
+            <Image
+              key={currentImage}
+              src={`https://res.cloudinary.com/de463zyga/image/upload/${currentImage}.png`}
+              alt={currentImage}
+              fill
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              className="object-contain"
+            />
+          )}
+        </div>
+      </div>
+
+      <div className="order-3 md:col-span-5 md:col-start-7 mt-6 md:mt-0">
+        <div className="hidden md:block">
+          <div className="flex md:justify-between items-end mb-1">
+            <h1 className="text-2xl md:text-3xl font-semibold text-gray-900">
+              {kitName}
+            </h1>
+            <h2 className="text-xl md:text-2xl font-medium text-primary">
+              ${kitPrice}
+            </h2>
           </div>
-          <p className="text-lg mb-2">
+          <p className="text-lg">
             {clubName} - {teamName}
           </p>
         </div>
 
-        <div className="order-2 md:order-1 md:col-span-1 md:col-start-1 md:row-start-1">
-          <div
-            className={cn(
-              "flex md:grid md:grid-rows-4 gap-2",
-              "overflow-x-auto md:overflow-x-visible",
-              "my-4 md:my-0 space-x-2 md:space-x-0",
-            )}
-          >
-            {images.map((img, index) => (
-              <div
-                key={index}
-                onClick={() => setCurrentImage(img)}
-                className={cn(
-                  currentImage === img
-                    ? "border border-primary/30 shadow-md"
-                    : "border border-gray-200",
-                  "relative w-16 md:w-full aspect-square flex-shrink-0 md:flex-shrink",
-                  "overflow-hidden rounded-md cursor-pointer",
-                )}
+        <form onSubmit={(e) => e.preventDefault()} className="space-y-6">
+          <div className="flex items-center justify-between my-6">
+            <div>
+              <label className="block text-sm font-medium text-gray-700 mb-2">
+                Kit Color
+              </label>
+              <div className="flex space-x-2">
+                {colors.map((color) => (
+                  <button
+                    key={color}
+                    onClick={() => handleColorChange(color)}
+                    className={cn(
+                      `w-8 h-8 rounded-full ${color.toLowerCase()}-color border border-gray-300`,
+                      selectedColor === color
+                        ? "ring-2 ring-blue-500 ring-offset-2"
+                        : "",
+                    )}
+                    aria-label={color}
+                    style={{ backgroundColor: color.toLowerCase() }}
+                  />
+                ))}
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="player-select"
+                className="block text-sm font-medium text-gray-700 mb-1"
               >
-                <Image
-                  src={`https://res.cloudinary.com/de463zyga/image/upload/${img}.png`}
-                  alt={img}
-                  fill
-                  className="object-contain"
-                />
+                Select Player
+              </label>
+              <Select
+                onValueChange={(value) => handlePlayerChange(value)}
+                value={selectedPlayer}
+              >
+                <SelectTrigger
+                  id="player-select"
+                  className={cn(
+                    "w-full",
+                    errors.player ? "border-red-500 ring-red-500" : "",
+                  )}
+                >
+                  <SelectValue placeholder="Choose a player..." />
+                </SelectTrigger>
+                <SelectContent>
+                  {players.map((player) => (
+                    <SelectItem key={player.id} value={player.name}>
+                      {player.name}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+                {errors.player && (
+                  <p className="absolute text-red-500 text-sm">
+                    Please select a player.
+                  </p>
+                )}
+              </Select>
+            </div>
+          </div>
+
+          <div className="my-6 space-y-4">
+            {products.map((product) => (
+              <div
+                key={product.id}
+                className="flex items-center space-x-4 bg-white p-4 rounded-lg shadow-sm border border-gray-200"
+              >
+                <div className="relative w-16 md:w-20 aspect-square flex-shrink-0">
+                  {currentProductImages[product.id] && (
+                    <Image
+                      src={currentProductImages[product.id]}
+                      alt={product.images[0].alt}
+                      fill
+                      className="object-cover rounded-md"
+                    />
+                  )}
+                </div>
+                <div className="flex-grow">
+                  <h3 className="text-sm font-medium text-gray-900">
+                    {product.name}
+                  </h3>
+                  <p className="text-xs text-gray-500">${product.price}</p>
+                </div>
+                <div>
+                  <Select
+                    onValueChange={(value) =>
+                      handleSizeChange(product.id, value)
+                    }
+                    value={selectedProducts[product.id] || ""}
+                  >
+                    <SelectTrigger
+                      className={cn(
+                        "w-[100px]",
+                        errors.products[product.id]
+                          ? "border-red-500 ring-red-500"
+                          : "",
+                      )}
+                    >
+                      <SelectValue placeholder="Size" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {product.attributes
+                        .find((attr) => attr.name.toLowerCase() === "size")
+                        ?.options.map((size) => (
+                          <SelectItem key={size} value={size}>
+                            {size}
+                          </SelectItem>
+                        ))}
+                    </SelectContent>
+                  </Select>
+                  {errors.products[product.id] && (
+                    <p className="absolute text-red-500 text-sm -translate-x-6">
+                      Please select a size.
+                    </p>
+                  )}
+                </div>
               </div>
             ))}
           </div>
-        </div>
-
-        <div className="order-1 md:order-2 md:col-span-5 md:col-start-2 md:row-start-1">
-          <div className="relative w-full aspect-square md:aspect-[2/3] overflow-hidden rounded-xl">
-            {currentImage && (
-              <Image
-                key={currentImage}
-                src={`https://res.cloudinary.com/de463zyga/image/upload/${currentImage}.png`}
-                alt={currentImage}
-                fill
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                className="object-contain"
-              />
-            )}
-          </div>
-        </div>
-
-        <div className="order-3 md:col-span-5 md:col-start-7 mt-6 md:mt-0">
-          <div className="hidden md:block">
-            <div className="flex md:justify-between items-end mb-1">
-              <h1 className="text-2xl md:text-3xl font-semibold text-gray-900">
-                {kitName}
-              </h1>
-              <h2 className="text-xl md:text-2xl font-medium text-primary">
-                ${kitPrice}
-              </h2>
-            </div>
-            <p className="text-lg">
-              {clubName} - {teamName}
-            </p>
-          </div>
-
-          <form onSubmit={(e) => e.preventDefault()} className="space-y-6">
-            <div className="flex items-center justify-between my-6">
-              <div>
-                <label className="block text-sm font-medium text-gray-700 mb-2">
-                  Kit Color
-                </label>
-                <div className="flex space-x-2">
-                  {colors.map((color) => (
-                    <button
-                      key={color}
-                      onClick={() => handleColorChange(color)}
-                      className={cn(
-                        `w-8 h-8 rounded-full ${color.toLowerCase()}-color border border-gray-300`,
-                        selectedColor === color
-                          ? "ring-2 ring-blue-500 ring-offset-2"
-                          : "",
-                      )}
-                      aria-label={color}
-                      style={{ backgroundColor: color.toLowerCase() }}
-                    />
-                  ))}
-                </div>
-              </div>
-              <div>
-                <label
-                  htmlFor="player-select"
-                  className="block text-sm font-medium text-gray-700 mb-1"
-                >
-                  Select Player
-                </label>
-                <Select
-                  onValueChange={(value) => handlePlayerChange(value)}
-                  value={selectedPlayer}
-                >
-                  <SelectTrigger
-                    id="player-select"
-                    className={cn(
-                      "w-full",
-                      errors.player ? "border-red-500 ring-red-500" : "",
-                    )}
-                  >
-                    <SelectValue placeholder="Choose a player..." />
-                  </SelectTrigger>
-                  <SelectContent>
-                    {players.map((player) => (
-                      <SelectItem key={player.id} value={player.name}>
-                        {player.name}
-                      </SelectItem>
-                    ))}
-                  </SelectContent>
-                  {errors.player && (
-                    <p className="absolute text-red-500 text-sm">
-                      Please select a player.
-                    </p>
-                  )}
-                </Select>
-              </div>
-            </div>
-
-            <div className="my-6 space-y-4">
-              {products.map((product) => (
-                <div
-                  key={product.id}
-                  className="flex items-center space-x-4 bg-white p-4 rounded-lg shadow-sm border border-gray-200"
-                >
-                  <div className="relative w-16 md:w-20 aspect-square flex-shrink-0">
-                    {currentProductImages[product.id] && (
-                      <Image
-                        src={currentProductImages[product.id]}
-                        alt={product.images[0].alt}
-                        fill
-                        className="object-cover rounded-md"
-                      />
-                    )}
-                  </div>
-                  <div className="flex-grow">
-                    <h3 className="text-sm font-medium text-gray-900">
-                      {product.name}
-                    </h3>
-                    <p className="text-xs text-gray-500">${product.price}</p>
-                  </div>
-                  <div>
-                    <Select
-                      onValueChange={(value) =>
-                        handleSizeChange(product.id, value)
-                      }
-                      value={selectedProducts[product.id] || ""}
-                    >
-                      <SelectTrigger
-                        className={cn(
-                          "w-[100px]",
-                          errors.products[product.id]
-                            ? "border-red-500 ring-red-500"
-                            : "",
-                        )}
-                      >
-                        <SelectValue placeholder="Size" />
-                      </SelectTrigger>
-                      <SelectContent>
-                        {product.attributes
-                          .find((attr) => attr.name.toLowerCase() === "size")
-                          ?.options.map((size) => (
-                            <SelectItem key={size} value={size}>
-                              {size}
-                            </SelectItem>
-                          ))}
-                      </SelectContent>
-                    </Select>
-                    {errors.products[product.id] && (
-                      <p className="absolute text-red-500 text-sm -translate-x-6">
-                        Please select a size.
-                      </p>
-                    )}
-                  </div>
-                </div>
-              ))}
-            </div>
-            <AddKitToCartButton
-              kitName={kitName}
-              kitPrice={kitPrice}
-              kitImage={currentImage}
-              selectedPlayer={selectedPlayer}
-              selectedColor={selectedColor}
-              selectedProducts={selectedProducts}
-              teamName={teamName}
-              products={products}
-              setErrors={setErrors}
-            />
-          </form>
-        </div>
+          <AddKitToCartButton
+            kitName={kitName}
+            kitPrice={kitPrice}
+            kitImage={currentImage}
+            selectedPlayer={selectedPlayer}
+            selectedColor={selectedColor}
+            selectedProducts={selectedProducts}
+            teamName={teamName}
+            products={products}
+            setErrors={setErrors}
+          />
+        </form>
       </div>
-    </section>
+    </div>
   );
 }
