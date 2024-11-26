ca828b666d2e97bd11b31b4c0e9cf42d90b9f27f
Isai Sanzy <isai10sanchez@gmail.com>
2024-11-26 15:15:41 -0700

    added metadata and description properties to the payment intent object creation to show product information after purchase in Stripe dashboard such as what item was purchased and attributes if any

--- src/app/api/create-payment-intent/route.ts
+++ src/app/api/create-payment-intent/route.ts
@@ -3,12 +3,21 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 export async function POST(request: NextRequest) {
   try {
-    const { amount } = await request.json();
+    const { amount, metadata, description } = await request.json();
+
+    // validate metadata size since Stripe has a limit
+    const metadataSize = JSON.stringify(metadata).length;
+    if (metadataSize > 50000) {
+      console.log(metadataSize);
+      throw new Error("metadata exceeds Stripe size limit");
+    }
 
     const paymentIntent = await stripe.paymentIntents.create({
       amount: amount,
       currency: "usd",
       automatic_payment_methods: { enabled: true },
+      metadata: metadata,
+      description: description,
     });
 
     return NextResponse.json({ clientSecret: paymentIntent.client_secret });
