f1bb0873c7bb1bd7317590217ee7df2c9e7441dd
Isai Sanzy <isai10sanchez@gmail.com>
2024-11-26 12:14:13 -0700

    new checkout directory for checkout components, for now it contains the checkout form component which handles creating the stripe client, using their custom hooks and rendering their form UI with error messages and other ui elements

---
+++ src/components/checkout/checkout-form.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import {
+  useStripe,
+  useElements,
+  PaymentElement,
+} from "@stripe/react-stripe-js";
+import convertToSubcurrency from "@/lib/stripe/convertToSubcurrency";
+import { Button } from "@/components/ui/button";
+
+const CheckoutForm = ({ amount }: { amount: number }) => {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const [errorMessage, setErrorMessage] = useState<string>();
+  const [clientSecret, setClientSecret] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  // generate client every time amount changes by sending POST request to the API route handler
+  useEffect(() => {
+    fetch("/api/create-payment-intent", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ amount: convertToSubcurrency(amount) }),
+    })
+      .then((res) => res.json())
+      .then((data) => setClientSecret(data.clientSecret));
+  }, [amount]);
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setLoading(true);
+
+    if (!stripe || !elements) {
+      return;
+    }
+
+    const { error: submitError } = await elements.submit();
+
+    if (submitError) {
+      setErrorMessage(submitError.message);
+      setLoading(false);
+      return;
+    }
+
+    const { error } = await stripe.confirmPayment({
+      elements,
+      clientSecret,
+      confirmParams: {
+        return_url: `http://www.localhost:3000/payment-success?amount=${amount}`,
+      },
+    });
+
+    if (error) {
+      // This point is only reached if there's an immediate error when confirming the payment
+      // Show the error to your customer (for example, payment details incomplete)
+      setErrorMessage(error.message);
+    } else {
+      // The payment UI automatically closes with a success animation at which point
+      // your customer is redirected to your "return_url"
+    }
+
+    setLoading(false);
+  };
+
+  if (!clientSecret || !stripe || !elements) {
+    return (
+      <div className="flex items-center justify-center">
+        <div
+          className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
+          role="status"
+        >
+          <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+            Loading...
+          </span>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      {clientSecret && <PaymentElement />}
+      {errorMessage && <div>{errorMessage}</div>}
+      <Button
+        className="w-full py-5 mt-4 text-white"
+        type="submit"
+        disabled={!stripe || loading}
+      >
+        {loading ? "Processing..." : `Pay $${amount.toFixed(2)}`}
+      </Button>
+    </form>
+  );
+};
+
+export default CheckoutForm;
