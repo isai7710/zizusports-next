3e185a7d0427b7a7579642dbbc52846367c43d56
Isai Sanzy <isai10sanchez@gmail.com>
2024-11-27 15:30:47 -0700

    added some small comments and better way to import stripe without ide giving me error

--- src/app/api/create-payment-intent/route.ts
+++ src/app/api/create-payment-intent/route.ts
@@ -1,5 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
 export async function POST(request: NextRequest) {
   try {
@@ -12,6 +14,7 @@ export async function POST(request: NextRequest) {
       throw new Error("metadata exceeds Stripe size limit");
     }
 
+    // CREATE PAYMENT INTENT
     const paymentIntent = await stripe.paymentIntents.create({
       amount: amount,
       currency: "usd",
@@ -20,6 +23,7 @@ export async function POST(request: NextRequest) {
       description: description,
     });
 
+    // RETURN CLIENT SECRET
     return NextResponse.json({ clientSecret: paymentIntent.client_secret });
   } catch (error) {
     console.error("Internal Error:", error);
