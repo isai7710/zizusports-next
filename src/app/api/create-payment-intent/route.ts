fd259e63fdafd0a5994bf41624672055ab4ffcbe
Isai Sanzy <isai10sanchez@gmail.com>
2024-12-02 16:23:03 -0700

    added more logic and sections to implement more secure and robust api practices like origin validation but yet to implement

--- src/app/api/create-payment-intent/route.ts
+++ src/app/api/create-payment-intent/route.ts
@@ -1,18 +1,45 @@
 import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
+//import { headers } from "next/headers";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
 export async function POST(request: NextRequest) {
   try {
-    const { amount, metadata, description } = await request.json();
+    // 1. Request Validation
+    /*
+    const headersList = headers();
+    const origin = headersList.get("origin");
+    if (!origin || origin !== process.env.NEXT_PUBLIC_APP_URL) {
+      return NextResponse.json(
+        { error: "Error: Invalid Origin Request" },
+        { status: 403 },
+      );
+    }
+    */
 
-    // validate metadata size since Stripe has a limit
+    // 2? Rate limiting implemented here
+
+    // 3. Input validation
+    const { amount, metadata, description } = await request.json();
+    if (!amount || !metadata || !description) {
+      return NextResponse.json(
+        { error: "Error: Missing Required Fields" },
+        { status: 400 },
+      );
+    }
+
+    // 4. Validate metadata size since Stripe has a limit
     const metadataSize = JSON.stringify(metadata).length;
     if (metadataSize > 50000) {
-      console.log(metadataSize);
-      throw new Error("metadata exceeds Stripe size limit");
+      return NextResponse.json(
+        { error: "Error: Metadata size is too large" },
+        { status: 400 },
+      );
     }
+
+    // 5. Server-side Cart Validation
+    // const validation = await validateCartPrices(items, amount);
 
     // CREATE PAYMENT INTENT
     const paymentIntent = await stripe.paymentIntents.create({
@@ -26,7 +53,7 @@ export async function POST(request: NextRequest) {
     // RETURN CLIENT SECRET
     return NextResponse.json({ clientSecret: paymentIntent.client_secret });
   } catch (error) {
-    console.error("Internal Error:", error);
+    console.error("Payment Intent Creation Error:", error);
     // Handle other errors (e.g. network issues, parsing errors, etc)
     return NextResponse.json(
       { error: `Internal Server Error: ${error}` },
